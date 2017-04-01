9c4598167e380b2d7035b9f7c1fc15d4a08b7dc4
Matt <mccormackm14@gmail.com>
2017-04-01 16:11:55 -0700

    Again

--- php/showPost.php
+++ php/showPost.php
@@ -1,19 +1,35 @@
+<html>
 <?php
 
-$sql = "Select * FROM post WHERE username LIKE '$username'";
-
-$stmt = mysqli_prepare($connection, $sql);
-$stmt->execute();
-
-$results = $stmt->get_result();
-
-while($row = mysqli_fetch_assoc($results)) {
-
-
-    echo $row['postID'] . "<br>";
-    echo $row['vote'] . "<br>";
-    echo $row['username'] . "<br>";
-    echo $row['content'] . "<br>";
-    echo $row['title'] . "<br>";
-
+if (isset($_SESSION['login_user'])) {
+    $username = $_SESSION['login_user'];
 }
+
+include 'connection.php';
+
+if ($error != null) {
+    $output = "<p>Unable to connect to database!</p>";
+    exit($output);
+} else {
+
+    $sql = "Select * FROM post WHERE username LIKE '$username'";
+
+    $stmt = mysqli_prepare($connection, $sql);
+    $stmt->execute();
+
+    $results = $stmt->get_result();
+
+    while ($row = mysqli_fetch_assoc($results)) {
+
+
+        echo "<br>" .$row['postID']. "<br>";
+        echo $row['username']. "<br>";
+        echo $row['content']. "<br>";
+        echo $row['title']. "<br>";
+
+    }
+}
+
+?>
+
+</html>
